2eca2d08eeefdcc27bc338cbaad57f0f40181d22
onyemachiprosper38-debug <onyemachiprosper38@gmail.com>
2026-05-18 17:13:26 +0100

    Add files via upload

---
+++ JAVA/Kata.java
@@ -0,0 +1,14 @@
+public class Kata {
+
+    public static void main(String[] args){
+
+for (int count = 1; count <= 10; count++){
+
+System.out.println("\nHello world!! ");
+System.out.print(" ");
+
+System.out.print(" ");
+
+   }
+  }
+ }
---
+++ JAVASCRIPT/Kata.js
@@ -0,0 +1,4 @@
+for(let count = 1; count <= 10; count++)
+console.log("\nHello world!! ")
+console.log(" ")
+console.log(" ")
