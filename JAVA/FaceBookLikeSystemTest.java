5a0a6eb0b44f6b56a2b2a44dcce285b332a36bc5
onyemachiprosper38-debug <onyemachiprosper38@gmail.com>
2026-05-20 12:26:40 +0100

    Day three

---
+++ JAVA/FaceBookLikeSystemTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.test;
+import org.junit,jupiter.api.assertion*;
+
+import FaceBookLikeSystem;
+
+    public class FaceBookLikeSystemTest
+
+    @test
+    public static void testThatLikesReturns(){
+    String[] = ["Peter"];
+    assertEquals("Peter likes");
+    }
+
+    @test
+    public static void testThatLikesReturns(){
+    String[] = ["Jacob", "Alex"];
+    assertEquals("Jacob and Alex likes this");
+    }
+
+     @test
+    public static void testThatLikesReturns(){
+    String[] = ["Max", "John", "Mark"];
+    assertEquals("Max, John and Mark likes this");
+    }
+
+     @test
+    public static void testThatLikesReturns(){
+    String[] = ["Alex", "Jacob" , "Mark" , "Max"];
+    assertEquals("Alex, Jacob and 2 others likes this");
+    }
+
+    
